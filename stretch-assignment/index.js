89ee20ea32dac053acbdaafb4572321f59dd6b24
Ryan State <rwstate@gmail.com>
2019-10-01 12:59:41 -0400

    finish stretch stretch goals

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,10 +1,14 @@
 let rockets = document.querySelectorAll(".block");
 let blastIndex = 0
 let intervalRun = ''
+let travels = 0
 rockets.forEach(rocket => {
     rocket.addEventListener("mousedown", event => {
-        let travels = 0
+        clearInterval(intervalRun)
         intervalRun = setInterval(function(){
+            if (travels > 800) {
+                clearInterval(intervalRun)
+            }
             travels += 1
             event.target.style.marginLeft = `${travels}px`
         }, 10)
@@ -14,7 +18,13 @@ rockets.forEach(rocket => {
         clearInterval(intervalRun)
         blastIndex += 1
         event.target.style.order = -blastIndex
-        travels = 0
+        intervalRun = setInterval(function(){
+            if (travels === 0) {
+                clearInterval(intervalRun)
+            }
+            travels -= 1
+            event.target.style.marginLeft = `${travels}px`
+        }, 10)
     })
 })
 
